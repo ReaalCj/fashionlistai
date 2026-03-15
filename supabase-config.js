acbb34ab6fe41e9b75a4c8fb89e82dbccb80f8ad
ReaalCj <163397569+ReaalCj@users.noreply.github.com>
2026-03-15 17:41:35 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 // Fill these with your Supabase project settings (see instructions below)
-export const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+export const SUPABASE_URL = "https://mxxwfxughzznmjfsnhvo.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14eHdmeHVnaHp6bm1qZnNuaHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTA0MTksImV4cCI6MjA4OTE2NjQxOX0.rNemEgiRG3Pn153ig9marPaaCykrgx8o5EWhDqZMA1o";
